a67ab167a83a31832c1c3bb28bf4ab0f719d0cd1
wolfen <wolfen@wolfen.za.net>
2022-01-23 14:58:29 +1300

    publish data to home assistant

--- mqtt_control.py
+++ mqtt_control.py
@@ -27,12 +27,17 @@ class MQTTControl():
         self.home_assistant_configure()
 
     def home_assistant_configure(self):
-        self.client.publish("%s/config" % self.homeAssistantUrl, '{"name":"temp_sensor_%s","dev_cla":"temperature","stat_t":"%s/state","unit_of_meas":"C","val_tpl":"{{value_json.temperature}}"}' % (self.client_id.decode('ascii'), self.homeAssistantUrl) )
+        self.client.publish("%s_temp/config" % self.homeAssistantUrl, '{"name":"Temperature Sensor %s Reading", "dev_cla":"temperature","stat_t":"%s/state","unit_of_meas":"C","val_tpl":"{{value_json.temperature}}"}' % (self.client_id.decode('ascii'), self.homeAssistantUrl) )
+        self.client.publish("%s_rssi/config" % self.homeAssistantUrl, '{"name":"Temperature Sensor %s Wifi", "dev_cla":"signal_strength","stat_t":"%s/state","unit_of_meas":"dBm","val_tpl":"{{value_json.rssi}}"}' % (self.client_id.decode('ascii'), self.homeAssistantUrl) )
+        self.client.publish("%s_ip/config" % self.homeAssistantUrl, '{"name":"Temperature Sensor %s IP", "dev_cla":"None","stat_t":"%s/state","val_tpl":"{{value_json.ip}}"}' % (self.client_id.decode('ascii'), self.homeAssistantUrl) )
+        self.client.publish("%s_ssid/config" % self.homeAssistantUrl, '{"name":"Temperature Sensor %s SSID", "dev_cla":"None","stat_t":"%s/state","val_tpl":"{{value_json.ssid}}"}' % (self.client_id.decode('ascii'), self.homeAssistantUrl) )
 
-    def home_assistant_status(self, temperature):
-        self.client.publish("%s/state" % self.homeAssistantUrl, '{"temperature":%s}' % (str(temperature)) )
+    def home_assistant_status(self, temperature, rssi, ip, ssid):
+        self.client.publish("%s/state" % self.homeAssistantUrl, '{"temperature":%s, "rssi":%s, "ip":"%s", "ssid": "%s"}' % ( str(temperature), str(rssi), ip, ssid ) )
 
     def post_status(self):
+
+        updateHA = False
 
         if (self.status == None):
             self.status = [None, None, None, time.time(), None, None, None, None, None, None, None, None, None, None]
@@ -40,24 +45,31 @@ class MQTTControl():
         if (self.status[0] != "true"):
             self.client.publish(self.topic_pub + b"/wifi/up", "true")
             self.status[0] = "true"
+            updateHA = True
 
         if (self.status[1] != self.sta_if.ifconfig()[0]):
             self.client.publish(self.topic_pub + b"/wifi/ip", self.sta_if.ifconfig()[0])
             self.status[1] = self.sta_if.ifconfig()[0]
+            updateHA = True
 
         if (self.status[2] != self.sta_if.config('essid')):
             self.client.publish(self.topic_pub + b"/wifi/ssid", self.sta_if.config('essid'))
             self.status[2] = self.sta_if.config('essid')
+            updateHA = True
 
         # Report RSSI every min
         if (self.status[3] < time.time()):
             self.client.publish(self.topic_pub + b"/wifi/rssi", str(self.sta_if.status('rssi')))
             self.status[3] = time.time() + 60
+            updateHA = True
 
         if (self.status[4] != self.temp.currentTemp()):
             self.client.publish(self.topic_pub + b"/temp/current", str(self.temp.currentTemp()))
             self.status[4] = self.temp.currentTemp()
-            self.home_assistant_status(self.temp.currentTemp())
+            updateHA = True
+
+        if updateHA:
+            self.home_assistant_status(self.temp.currentTemp(), self.sta_if.status('rssi'), self.sta_if.ifconfig()[0], self.sta_if.config('essid'))
 
     def start(self, temp):
 
