d4db9842865dbb4342fc24c3b6333922dc68d1f6
Wolfen <gitlab@wolfen.za.net>
2022-02-01 16:16:16 +1300

    thingsboard integration working :)

--- thingsboard_control.py
+++ thingsboard_control.py
@@ -28,11 +28,6 @@ class ThingsboardControl(BasicModule):
         settings.load()
         self.enabled = settings.Enable
         self.mqtt_server = settings.Server
-        if (settings.Subscribe != b""):
-            self.topic_sub = settings.Subscribe
-        else:
-            self.topic_sub = b'thingsboard/sensor/%s/command/#' % (self.client_id)
-
         if (settings.Publish != b""):
             self.topic_pub = settings.Publish
         else:
@@ -46,10 +41,10 @@ class ThingsboardControl(BasicModule):
                 try:
                     if (not self.init):
                         self.init = True
-                        self.connect_and_subscribe()
+                        self.connect()
                     self.client.check_msg()
                 except Exception as e:
-                    self.connect_and_subscribe()
+                    self.connect()
                     raise
 
     def getTelemetry(self):
@@ -86,15 +81,10 @@ class ThingsboardControl(BasicModule):
 
     # Internal Code 
 
-    def sub_cb(self, topic, msg):
-        SerialLog.log("TB MQTT Command Received: ", topic, msg)
-
-    def connect_and_subscribe(self):
+    def connect(self):
         self.client = MQTTClient(b"tb-" + self.client_id, self.mqtt_server, port=int(self.mqtt_port), user=self.access_token, password=self.access_token)
-        self.client.set_callback(self.sub_cb)
         self.client.connect()
-        self.client.subscribe(self.topic_sub)
-        SerialLog.log('Connected to %s : %s TB MQTT broker, subscribed to %s topic' % (self.mqtt_server, str(self.mqtt_port), self.topic_sub))
+        SerialLog.log('Connected to %s : %s TB MQTT broker' % (self.mqtt_server, str(self.mqtt_port)))
 
 
     def settings(self, settingsVals):
@@ -103,47 +93,40 @@ class ThingsboardControl(BasicModule):
         self.mqtt_server = settingsVals[1]
 
         if (settingsVals[2] != b""):
-            self.topic_sub = settingsVals[2]
-        else:
-            self.topic_sub = b'thingsboard/sensor/%s/command/#' % (self.client_id)
-
-        if (settingsVals[3] != b""):
-            self.topic_pub = settingsVals[3]
+            self.topic_pub = settingsVals[2]
         else: 
             self.topic_pub = b"v1/devices/me/telemetry"
 
-        self.mqtt_port = settingsVals[4]
-        self.access_token = settingsVals[5]
+        self.mqtt_port = settingsVals[3]
+        self.access_token = settingsVals[4]
 
         # Save the settings to disk
         settings = ThingsboardSettings()
         settings.Enable = self.enabled
         settings.Server = self.mqtt_server
-        settings.Subscribe = self.topic_sub
         settings.Publish = self.topic_pub
         settings.Port = self.mqtt_port
         settings.AccessToken = self.access_token
         settings.write()
     
     def getsettings(self):
-        s = (self.enabled, self.mqtt_server, self.topic_sub, self.topic_pub, self.mqtt_port, self.access_token)
+        s = (self.enabled, self.mqtt_server, self.topic_pub, self.mqtt_port, self.access_token)
         return s
 
     def loadtbsettings(self, params):
         settings =  self.getsettings()
         headers = okayHeader
-        data = b"{ \"enable\": \"%s\", \"server\": \"%s\", \"subscribe\": \"%s\", \"publish\": \"%s\", \"port\": %s, \"accesstoken\": \"%s\" }" % (settings[0], settings[1], settings[2], settings[3], str(settings[4]), settings[5])
+        data = b"{ \"enable\": \"%s\", \"server\": \"%s\", \"publish\": \"%s\", \"port\": %s, \"accesstoken\": \"%s\" }" % (settings[0], settings[1], settings[2], str(settings[3]), settings[4])
         return data, headers
 
     def savetbsettings(self, params):
         # Read form params
         enable = unquote(params.get(b"enable", None))
         server = unquote(params.get(b"server", None))
-        subscribe = unquote(params.get(b"subscribe", None))
         publish = unquote(params.get(b"publish", None))
         port = int(unquote(params.get(b"port", None)))
         accessToken = unquote(params.get(b"accesstoken", None))
-        settings = (enable, server, subscribe, publish, port, accessToken)
+        settings = (enable, server, publish, port, accessToken)
         self.settings(settings)
         headers = b"HTTP/1.1 307 Temporary Redirect\r\nLocation: /\r\n"
         return b"", headers
--- thingsboard_settings.py
+++ thingsboard_settings.py
@@ -5,10 +5,9 @@ class ThingsboardSettings:
 
     SETTINGS_FILE = "./tb.settings"
 
-    def __init__(self, Enable=b"Y", Server=b"mqtt.wolfen.za.net", Subscribe=b"", Publish=b"", Port=1883, AccessToken=b""):
+    def __init__(self, Enable=b"Y", Server=b"mqtt.wolfen.za.net", Publish=b"", Port=1883, AccessToken=b""):
         self.Enable=Enable
         self.Server=Server
-        self.Subscribe=Subscribe
         self.Publish=Publish
         self.Port = Port
         self.AccessToken = AccessToken
@@ -17,7 +16,7 @@ class ThingsboardSettings:
         """Write settings to settings_file if valid input found."""
         if self.is_valid():
             with open(self.SETTINGS_FILE, "wb") as f:
-                f.write(b",".join([self.Enable, self.Server, self.Subscribe, self.Publish, str(self.Port).encode('ascii'), self.AccessToken]))
+                f.write(b",".join([self.Enable, self.Server, self.Publish, str(self.Port).encode('ascii'), self.AccessToken]))
             SerialLog.log("Wrote settings to {:s}".format(self.SETTINGS_FILE))
         else:
             SerialLog.log("Invalid!")
@@ -27,13 +26,12 @@ class ThingsboardSettings:
             with open(self.SETTINGS_FILE, "rb") as f:
                 contents = f.read().split(b",")
             SerialLog.log("Loaded settings from {:s}".format(self.SETTINGS_FILE))
-            if len(contents) == 6:
+            if len(contents) == 5:
                 self.Enable = contents[0]
                 self.Server = contents[1]
-                self.Subscribe = contents[2]
-                self.Publish = contents[3]
-                self.Port = int(contents[4])
-                self.AccessToken = contents[5]
+                self.Publish = contents[2]
+                self.Port = int(contents[3])
+                self.AccessToken = contents[4]
 
             if not self.is_valid():
                 self.remove()
@@ -57,7 +55,6 @@ class ThingsboardSettings:
             pass
         self.Enable = b"Y"
         self.Server = b"mqtt.wolfen.za.net"
-        self.Subscribe = b""
         self.Publish = b""
         self.Port = 1883
         self.AccessToken = b""
@@ -67,8 +64,6 @@ class ThingsboardSettings:
         if not isinstance(self.Enable, bytes):
             return False
         if not isinstance(self.Server, bytes):
-            return False
-        if not isinstance(self.Subscribe, bytes):
             return False
         if not isinstance(self.Publish, bytes):
             return False
