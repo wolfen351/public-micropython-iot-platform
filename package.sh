0bdaedcb082b7b5651a0bf5841aea2dababe6094
Wolfen <gitlab@wolfen.za.net>
2022-02-11 15:31:35 +1300

    Update packager to deal with folders

--- package.sh
+++ package.sh
@@ -4,7 +4,9 @@
 ./bump_version.sh
 
 # make firmware archive for ota
-tar -czf firmware.tar.gz *.py *.html *.sh *.js *.cfg version
+shopt -s extglob nullglob globstar
+
+tar -czf firmware.tar.gz **/*.py **/*.html **/*.sh **/*.js **/*.cfg version
 V="$(cat version);firmware.tar.gz;30;$(sha256sum firmware.tar.gz | cut -d " " -f 1)"
 echo "Firmware latest: $V"
 echo $V > latest
