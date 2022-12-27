b7dad7cea19f1efe2421bf1cc16bc400d2158f14
Iain Prior <wolfen@wolfen.nz>
2022-12-28 11:29:24 +1300

    only set time if we have wifi

--- modules/binary_clock/binary_clock.py
+++ modules/binary_clock/binary_clock.py
@@ -8,6 +8,7 @@ from modules.touchscreen.xpt2046 import Touch
 from serial_log import SerialLog
 import ntptime
 import time
+import network
 
 # Screen is 320x240 px
 # X is left to right on the small side (0-240)
@@ -33,23 +34,26 @@ class BinaryClock(BasicModule):
         self.spi = SPI(1, baudrate=40000000, sck=Pin(7), mosi=Pin(11), miso=Pin(9))
         self.display = Display(self.spi, dc=Pin(12), cs=Pin(5), rst=Pin(0))
         self.display.clear(color565(0, 0, 0))
+        self.sta_if = network.WLAN(network.STA_IF)
 
     def tick(self):
-        if not self.gotTime:
-            # Set up NTP
-            try:
-                SerialLog.log("Local time before synchronization: %s" %str(time.localtime()))
-                #make sure to have internet connection
-                ntptime.settime()
-                SerialLog.log("Local time after synchronization: %s" %str(time.localtime(time.time() + self.UTC_OFFSET)))
-                doy = time.localtime()[-1]
-                if (doy < 92 or doy > 268): # 2 April, 25 Sept
-                    self.UTC_OFFSET += 3600
-                localTime = time.localtime(time.time() + self.UTC_OFFSET)
-                SerialLog.log("Local time after DST Calculation: %s" %str(localTime))
-                self.gotTime = True
-            except Exception as e:
-                SerialLog.log("Error syncing time: ", e)
+
+        if (not self.gotTime):
+            if (self.sta_if.isconnected()):
+                # Set up NTP
+                try:
+                    SerialLog.log("Local time before synchronization: %s" %str(time.localtime()))
+                    #make sure to have internet connection
+                    ntptime.settime()
+                    SerialLog.log("Local time after synchronization: %s" %str(time.localtime(time.time() + self.UTC_OFFSET)))
+                    doy = time.localtime()[-1]
+                    if (doy < 92 or doy > 268): # 2 April, 25 Sept
+                        self.UTC_OFFSET += 3600
+                    localTime = time.localtime(time.time() + self.UTC_OFFSET)
+                    SerialLog.log("Local time after DST Calculation: %s" %str(localTime))
+                    self.gotTime = True
+                except Exception as e:
+                    SerialLog.log("Error syncing time: ", e)
         else:
             localTime = time.localtime(time.time() + self.UTC_OFFSET)
             spacing = 65
