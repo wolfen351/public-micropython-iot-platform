f2258e57893e333437f1f832cf4da6491703391c
Iain Prior <wolfen@wolfen.nz>
2022-12-28 01:26:19 +1300

    Displays local time in decimal, next up, display the pics

--- modules/binary_clock/binary_clock.py
+++ modules/binary_clock/binary_clock.py
@@ -9,9 +9,6 @@ from serial_log import SerialLog
 import ntptime
 import time
 
-#if needed, overwrite default time server
-ntptime.host = "0.nz.pool.ntp.org"
-
 # Screen is 320x240 px
 # X is left to right on the small side (0-240)
 # Y is up to down on the long side (0-320)
@@ -22,11 +19,10 @@ class BinaryClock(BasicModule):
     display = None
     xpt = None
     font = XglcdFont('modules/touchscreen/fonts/font25x57.c', 25, 57, 32, 97, 228)
-    mode = "OFF"
-    detectedTemp = 23
-    setpoint = 23
-
-    #84f703d7c4b2 
+    gotTime = False
+    ntptime.host = "0.nz.pool.ntp.org"
+    UTC_OFFSET = 12 * 60 * 60
+    previous = [0,0,0,0,0,0]
 
     def __init__(self, basicSettings):
         pass
@@ -38,21 +34,42 @@ class BinaryClock(BasicModule):
         self.display = Display(self.spi, dc=Pin(12), cs=Pin(5), rst=Pin(0))
         self.display.clear(color565(0, 0, 0))
 
-        # Low speed SPI for touch
-        self.spi = SPI(1, baudrate=2000000, sck=Pin(7), mosi=Pin(11), miso=Pin(9))
-        self.xpt = Touch(self.spi, cs=Pin(18))
-
-        # Set up NTP
-        try:
-            SerialLog.log("Local time before synchronization：%s" %str(time.localtime()))
-            #make sure to have internet connection
-            ntptime.settime()
-            SerialLog.log("Local time after synchronization：%s" %str(time.localtime()))
-        except:
-            SerialLog.log("Error syncing time")
-
     def tick(self):
-        pass
+        if not self.gotTime:
+            # Set up NTP
+            try:
+                SerialLog.log("Local time before synchronization: %s" %str(time.localtime()))
+                #make sure to have internet connection
+                ntptime.settime()
+                SerialLog.log("Local time after synchronization: %s" %str(time.localtime(time.time() + self.UTC_OFFSET)))
+                doy = time.localtime()[-1]
+                if (doy < 92 or doy > 268): # 2 April, 25 Sept
+                    self.UTC_OFFSET += 3600
+                localTime = time.localtime(time.time() + self.UTC_OFFSET)
+                SerialLog.log("Local time after DST Calculation: %s" %str(localTime))
+                self.gotTime = True
+            except Exception as e:
+                SerialLog.log("Error syncing time: ", e)
+        else:
+            localTime = time.localtime(time.time() + self.UTC_OFFSET)
+            spacing = 65
+            leftspacing = 60
+            if (self.previous[5] != localTime[5]):
+                self.drawNumber(localTime[5], 200, leftspacing) # seconds
+            if (self.previous[4] != localTime[4]):
+                self.drawNumber(localTime[4], 200, leftspacing+spacing) # mins
+            if (self.previous[3] != localTime[3]):
+                self.drawNumber(localTime[3], 200, leftspacing+spacing*2) # mins
+            self.previous = localTime
+
+
+    def drawNumber(self, number, x, y):
+        tens = (round(number) // 10) % 10 
+        ones = round(number) % 10 
+        self.display.draw_image('modules/ac_remote/small'+str(tens)+'.raw',x,y+25,36,24)
+        self.display.draw_image('modules/ac_remote/small'+str(ones)+'.raw',x,y,36,24)
+
+
 
     def getTelemetry(self):
         telemetry = {
