29e9ba81777a665af1160063df46aeddb588c942
Iain Prior <wolfen@wolfen.nz>
2022-12-30 22:45:41 +1300

    Get some history data into an array

--- modules/dht22/dht22.py
+++ modules/dht22/dht22.py
@@ -8,6 +8,7 @@ class Dht22Monitor(BasicModule):
 
     currentT = 0
     currentH = 0
+    historyT = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
     def __init__(self):
         self.lastConvert = time.ticks_ms()
@@ -26,11 +27,14 @@ class Dht22Monitor(BasicModule):
             self.currentT = self.dht.temperature()
             self.currentH = self.dht.humidity()
             self.lastConvert = currentTime
+            self.historyT.pop(0)
+            self.historyT.append(self.dht.temperature())
 
     #@micropython.native 
     def getTelemetry(self):
         telemetry = {
             "temperature/dht22" : self.currentT,
+            "temperature/dht22_history" : self.historyT,
             "humidity/dht22" : self.currentH
         }
         return telemetry
