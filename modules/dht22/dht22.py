a80baf66fa9fc0efb1da528bc6c354cbff48a944
Iain Prior <wolfen@wolfen.nz>
2022-12-31 14:08:40 +1300

    remove history calc

--- modules/dht22/dht22.py
+++ modules/dht22/dht22.py
@@ -8,7 +8,6 @@ class Dht22Monitor(BasicModule):
 
     currentT = 0
     currentH = 0
-    historyT = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
     def __init__(self):
         self.lastConvert = time.ticks_ms()
@@ -27,8 +26,6 @@ class Dht22Monitor(BasicModule):
             self.currentT = self.dht.temperature()
             self.currentH = self.dht.humidity()
             self.lastConvert = currentTime
-            self.historyT.pop(0)
-            self.historyT.append(self.dht.temperature())
 
     #@micropython.native 
     def getTelemetry(self):
