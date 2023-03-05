47ae1b9048034632e9831da0cb13c3e92700a24e
Iain Prior <wolfen@wolfen.nz>
2023-03-05 13:26:33 +1300

    Draw full image on time change

--- modules/binary_clock/binary_clock.py
+++ modules/binary_clock/binary_clock.py
@@ -49,6 +49,7 @@ class BinaryClock(BasicModule):
                     localTime = time.localtime(time.time() + self.UTC_OFFSET)
                     SerialLog.log("Local time after UTC Offet & DST Calculation: %s" %str(localTime))
                     self.gotTime = True
+                    self.displayTime(True)
                 except Exception as e:
                     SerialLog.log("Error syncing time: ", e)
 
@@ -58,19 +59,19 @@ class BinaryClock(BasicModule):
 
         self.displayTime()
 
-    def displayTime(self):
+    def displayTime(self, full = False):
         localTime = time.localtime(time.time() + self.UTC_OFFSET)
         spacing = 65
         leftspacing = 60
-        if (self.previous[5] != localTime[5]):
+        if (self.previous[5] != localTime[5] or full == True):
             self.drawNumber(localTime[5], 200, leftspacing, False) # seconds
-            self.drawBinary(localTime[5], 0, 10)
-        if (self.previous[4] != localTime[4]):
+            self.drawBinary(localTime[5], 0, 10, full)
+        if (self.previous[4] != localTime[4] or full == True):
             self.drawNumber(localTime[4], 200, leftspacing+spacing) # mins
-            self.drawBinary(localTime[4], 0, 110)
-        if (self.previous[3] != localTime[3]):
+            self.drawBinary(localTime[4], 0, 110, full)
+        if (self.previous[3] != localTime[3] or full == True):
             self.drawNumber(localTime[3], 200, leftspacing+spacing*2) # hours
-            self.drawBinary(localTime[3], 0, 210)
+            self.drawBinary(localTime[3], 0, 210, full)
         self.previous = localTime
 
     # draws a decimal number
@@ -83,24 +84,24 @@ class BinaryClock(BasicModule):
         self.display.draw_image('modules/binary_clock/small'+str(ones)+'.raw',x,y,36,24)
 
     # draws a whole decimal number like 23 as a set of binary digits with leds
-    def drawBinary(self, number, x, y):
+    def drawBinary(self, number, x, y, full):
         decimal_tens = (round(number) // 10) % 10 
         decimal_ones = round(number) % 10 
-        self.drawBCD(decimal_ones, x, y)
-        self.drawBCD(decimal_tens, x, y + 43)
+        self.drawBCD(decimal_ones, x, y, full)
+        self.drawBCD(decimal_tens, x, y + 43, full)
 
     # draws one decimal digit as a series of bit leds
-    def drawBCD(self, number, x, y):
+    def drawBCD(self, number, x, y, full):
         decimal_ones = number % 10
         ones = round(decimal_ones) % 2 
         twos = round(decimal_ones - ones) % 4 
         fours = round(decimal_ones - ones - twos) % 8 
         eights = round(decimal_ones - ones - twos - fours) % 16
-        if (number == 0 or number == 8):
+        if (number == 0 or number == 8 or full == True):
             self.drawBit(eights, x+10, y)
-        if (number == 0 or number == 4 or number == 8):
+        if (number == 0 or number == 4 or number == 8 or full == True):
             self.drawBit(fours, x+52, y)
-        if (number % 2 == 0):
+        if (number % 2 == 0 or full == True):
             self.drawBit(twos, x+94, y) 
         self.drawBit(ones, x+136, y)
 
