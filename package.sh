9c669253f3586f64880a65698067286fe1f447cc
Iain Prior <wolfen@wolfen.nz>
2023-06-09 16:34:47 +1200

    Start doing profile stuff

--- package.sh
+++ package.sh
@@ -7,3 +7,19 @@ nv=$(echo $v | awk -F. '{ printf("%d.%d.%d.%d",$1,$2,$3+1,0); }')
 newVersion=${nv%.*}
 echo "Version is now: $newVersion"
 echo $newVersion > ./version
+
+#make firmware archives for ota
+profiles=$(ls -1 ./profiles)
+
+#record the last time a file was edited
+MAXEDITTIME=$(echo "$MAXEDITTIME" | awk '{print int($1+0.5)}')
+echo $MAXEDITTIME > ./lastedit.dat
+echo "Max Edit Time: $MAXEDITTIME"
+
+for profile in $profiles
+do
+    echo "Processing Profile: $profile"
+done
+
+
+
