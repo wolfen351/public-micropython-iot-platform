b17effa0ec62b6f9417b0ddfc571438de6610f5e
Iain Prior <wolfen@wolfen.nz>
2023-06-10 19:28:44 +1200

    v6 gitlab pipeline id

--- package.sh
+++ package.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # increment the version
-raw=$(cat ./version)
-v=$(echo $raw | awk -F. '{ printf("%d.%d.%d",$1,$2,$3); }')
-nv=$(echo $v | awk -F. '{ printf("%d.%d.%d.%d",$1,$2,$3+1,0); }')
-newVersion=${nv%.*}
+newVersion=6.0.$CI_PIPELINE_ID
 echo "Version is now: $newVersion"
 echo $newVersion > ./version
 
