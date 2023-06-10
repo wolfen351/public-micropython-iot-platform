f20b9c180f8ffd0fd9fdf5da1b4096ec569cb0e9
Iain Prior <wolfen@wolfen.nz>
2023-06-10 13:19:35 +1200

    stable firmware file name

--- package.sh
+++ package.sh
@@ -82,7 +82,7 @@ do
     cd ..
 
     # Calculate a h256 hash of the files
-    h256 = $(sha256sum ./firmware.$vers.tar.gz | awk '{print $1}')
+    h256 = $(sha256sum ./firmware.tar.gz | awk '{print $1}')
     size=$(du -k ./firmware.tar.gz | awk '{print $1}') # size in kb
     V="$vers;firmware.tar.gz;$size;$h256"
     echo "Firmware latest: $V"
