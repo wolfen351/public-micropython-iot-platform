7c2193785338313db49d8cf762fc895a9e553165
Iain Prior <wolfen@wolfen.nz>
2023-06-10 13:30:37 +1200

    Fix location

--- package.sh
+++ package.sh
@@ -82,8 +82,8 @@ do
     cd ..
 
     # Calculate a h256 hash of the files
-    h256 = $(sha256sum ./firmware.tar.gz | awk '{print $1}')
-    size=$(du -k ./firmware.tar.gz | awk '{print $1}') # size in kb
+    h256 = $(sha256sum ../artifacts/$shortName/firmware.tar.gz | awk '{print $1}')
+    size=$(du -k ../artifacts/$shortName/firmware.tar.gz | awk '{print $1}') # size in kb
     V="$vers;firmware.tar.gz;$size;$h256"
     echo "Firmware latest: $V"
     echo $V > ./artifacts/$shortName/latest
