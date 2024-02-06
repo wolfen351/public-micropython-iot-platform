86ee09edb05541a6008a733db4191998c788027e
Iain Prior <wolfen@wolfen.nz>
2024-02-06 13:09:10 +1300

    Add firmware

--- flash_python_esp8266.ps1
+++ flash_python_esp8266.ps1
@@ -11,7 +11,7 @@ Write-Host "Press any key to continue..."
 $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
 
-esptool.py --port $port erase_flash
-esptool.py --port $port --baud 1000000 write_flash --flash_size=4MB -fm dio 0 .\firmware\esp8266-20230426-v1.20.0.bin
+esptool --port $port erase_flash
+esptool --port $port --baud 1000000 write_flash --flash_size=1MB -fm dio 0 .\firmware\esp8266-20230426-v1.20.0.bin
 
 Show-SerialLog
