4af851449a6d2cd4c4b57c29bb2ce4e77d03b6c0
Iain Prior <wolfen@wolfen.nz>
2024-02-16 16:18:04 +1300

    add sonoff basic r2 flashing profile (not working yet)

---
+++ flash_python_sonoffbasicr2.ps1
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+Write-Output "Set board into programming mode."
+Write-Output "    Hold the Button"
+Write-Output "    Connect the power"
+Write-Output "    Release Button when you hear the prompt tone on usb reconnection"
+
+Write-Host "Press any key to continue..."
+$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+
+Import-Module ./serial-toys.psm1
+$port = Find-MicrocontrollerPort
+Write-Output "Found port: $port"
+
+
+esptool --chip esp8266 --port $port erase_flash
+esptool --chip esp8266 --port $port --baud 115200 write_flash -z 0x0 ./firmware/ESP8266_GENERIC-20240105-v1.22.1.bin
