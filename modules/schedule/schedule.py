53d3a0c553042432c3cdb634436d9f7b5be37aa0
Iain Prior <wolfen@wolfen.nz>
2024-08-03 18:14:16 +1200

    add abaility to save and load schedule settings

--- modules/schedule/schedule.py
+++ modules/schedule/schedule.py
@@ -1,4 +1,5 @@
 from modules.basic.basic_module import BasicModule
+from web.web_processor import okayHeader, unquote
 
 class Schedule(BasicModule):
 
@@ -26,12 +27,32 @@ class Schedule(BasicModule):
 
     def getRoutes(self):
         return {
-            b"/schedule": b"/modules/schedule/settings.html"
+            b"/schedule": b"/modules/schedule/settings.html",
+            b"/scheduleloadsettings": self.loadschedulesettings,
+            b"/schedulesavesettings": self.saveschedulesettings
         }
-
 
     def getIndexFileName(self):
         return { "schedule" : "/modules/schedule/schedule_index.html" }
 
 
     # internal code
+    def loadschedulesettings(self, params):
+
+        bootcommand = self.getPref("schedule", "bootcommand", "/somecommand")
+        delaycommand = self.getPref("schedule", "delaycommand", "/somecommand")
+        delayseconds = self.getPref("schedule", "delayseconds", "300")
+
+        headers = okayHeader
+        data = b"{ \"bootcommand\": \"%s\", \"delaycommand\": \"%s\", \"delayseconds\": \"%s\" }" % (bootcommand, delaycommand, delayseconds)
+        return data, headers
+    
+    def saveschedulesettings(self, params):
+        # Read form params
+        self.setPref("schedule", "bootcommand", unquote(params.get(b"bootcommand", None)))
+        self.setPref("schedule", "delaycommand", unquote(params.get(b"delaycommand", None)))
+        self.setPref("schedule", "delayseconds", unquote(params.get(b"delayseconds", None)))
+
+        headers = b"HTTP/1.1 307 Temporary Redirect\r\nLocation: /\r\n"
+
+        return b"", headers, True
