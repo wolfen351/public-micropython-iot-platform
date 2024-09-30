26bcbfe27689737fea18135f28a5a25c41543c58
Iain Prior <wolfen@wolfen.nz>
2024-09-30 19:48:08 +1300

    espcam firmware

--- flash_python_espcam.ps1
+++ flash_python_espcam.ps1
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-Write-Output "Make ESPCAM boards into Device Firmware Upgrade (DFU) mode."
-Write-Output "    Connect UART to USB"
-Write-Output "    See this URL for conenction diagram: "
-Write-Output "    https://lastminuteengineers.com/getting-started-with-esp32-cam/"
+Write-Output "This will flash the ESPCAM with the latest firmware."
+
+Import-Module ./serial-toys.psm1
+$port = Find-MicrocontrollerPort
+Write-Output "Found port: $port"
 
 Write-Host "Press any key to continue..."
 $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
-esptool --chip esp32 --port COM8 erase_flash
-esptool --chip esp32 --port COM8 --baud 1000000 write_flash -z 0x1000 ./firmware/micropython_cmake_9fef1c0bd_esp32_idf4.x_ble_camera.bin
 
+esptool --port $port erase_flash
+esptool --chip auto --port $port write_flash -z 0x0 ./firmware/ESP32_GENERIC_S3-SPIRAM_OCT-20240602-v1.23.0.bin
+
+Show-SerialLog
