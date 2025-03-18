1d8ff3a5a754d91c8b906d357dec7a956addc19a
Iain Prior <wolfen@wolfen.nz>
2025-03-18 15:25:55 +1300

    add and fix the esp32 with spiram version

--- flash_python_esp32r4v3.ps1
+++ flash_python_esp32r4v3.ps1
@@ -16,5 +16,5 @@ Write-Host "Press any key to continue..."
 $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
 esptool --chip esp32 --port $port erase_flash
-esptool --chip esp32 --port $port write_flash -z 0x1000 ./firmware/ESP32_GENERIC-SPIRAM-20241129-v1.24.1.bin
-
+# 1.20 last version that works, after this the wifi is causing issues
+esptool --chip esp32 --port $port write_flash -z 0x1000 ./firmware/ESP32_GENERIC-SPIRAM-20230426-v1.20.0.bin
