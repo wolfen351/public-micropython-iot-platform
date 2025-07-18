5aa72bd67139f3fcdc49dd29846517ca96648a31
Iain Prior <wolfen@wolfen.nz>
2025-07-18 13:27:18 +1200

    fix uom code

--- modules/homeassistant/homeassistant_control.py
+++ modules/homeassistant/homeassistant_control.py
@@ -280,9 +280,9 @@ class HomeAssistantControl(BasicModule):
             devclass = devClassLookup.get(lookupkey, "")
             telemetryType = "sensor"
 
-            # if uom contains a * then replace it with a degree symbol
-            if uom and "*" in uom:
-                uom = uom.replace("*", chr(176))
+            # if uom contains *C then replace it with a degree symbol + C
+            if uom == "*C":
+                uom = chr(176) + "C"
                         
             if "/" in key:
                 name = "%s %s" % (name, key.split("/")[1])
